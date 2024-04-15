986b2736e83f759e6c3d199387c57216769a9589
AlejandroDojo <ultiimately@gmail.com>
2024-04-15 19:09:50 -0400

    Secuencia negativa

--- bucles.js
+++ bucles.js
@@ -5,10 +5,9 @@ function impar() {
             console.log(i);
         };
     };
-};
-impar();
+}
 //  Usando un bucle, escribe un código que haga que console.log
-//imprima todos los valores que sean divisibles de manera uniforme por 3 del 100 al 0.
+// imprima todos los valores que sean divisibles de manera uniforme por 3 del 100 al 0.
 function multipleOfThree() {
     let i = 0;
     while (i <= 100) {
@@ -18,5 +17,12 @@ function multipleOfThree() {
         i++;
     };
 
-};
-multipleOfThree();
+}
+// Usando un bucle, escribe un código que haga que console.log
+// imprima los valores en esta secuencia  4, 2.5, 1, -0.5, -2, -3.5.
+function secuences() {
+    for (let i = 4; i >= -3.5; i-=1.5) {
+        console.log(i);
+    };
+}
+secuences();
