f3ac0107106973138a0937dd3486037459c74871
AlejandroDojo <ultiimately@gmail.com>
2024-04-15 18:59:37 -0400

    Multiplos de 3

--- bucles.js
+++ bucles.js
@@ -2,8 +2,21 @@
 function impar() {
     for (let i = 1; i < 21 ; i++) {
         if (i  % 2 === 1) {
-            console.log(i)
-        }
-    }
-}
-impar()
+            console.log(i);
+        };
+    };
+};
+impar();
+//  Usando un bucle, escribe un código que haga que console.log
+//imprima todos los valores que sean divisibles de manera uniforme por 3 del 100 al 0.
+function multipleOfThree() {
+    let i = 0;
+    while (i <= 100) {
+        if (i % 3  === 0) {
+            console.log(i);
+        };
+        i++;
+    };
+
+};
+multipleOfThree();
