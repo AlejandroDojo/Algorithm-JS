2f24279324313f4c662862573678c1318feece20
AlejandroDojo <ultiimately@gmail.com>
2024-04-15 12:45:59 -0400

    Impar Comentary

--- bucles.js
+++ bucles.js
@@ -1,3 +1,4 @@
+// Usando un bucle, escribe un código que haga que console.logimprima todos los valores impares del 1 al 20.
 function impar() {
     for (let i = 1; i < 21 ; i++) {
         if (i  % 2 === 1) {
@@ -6,4 +7,3 @@ function impar() {
     }
 }
 impar()
- 
