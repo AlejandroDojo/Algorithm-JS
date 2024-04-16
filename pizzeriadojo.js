70b234c434eb6b61a5cca1e55d14022c7782249e
AlejandroDojo <ultiimately@gmail.com>
2024-04-15 21:19:37 -0400

    Pizza Tradicional

---
+++ pizzeriadojo.js
@@ -0,0 +1,16 @@
+// Funcion para crear un objeto pizza
+function pizzaOven(tipoCorteza, tipoSalsa, quesos, salsas) {
+    var pizza = {};
+    pizza.tipoCorteza = tipoCorteza;
+    pizza.tipoSalsa = tipoSalsa;
+    pizza.quesos= quesos;
+    pizza.salsas = salsas;
+    return pizza;
+}
+// Se crea un objeto de Pizza Tradicional
+var pizzaTradicional = pizzaOven(
+    "estilo Chicago",
+    "tradicional",
+    ["mozarrella"],
+    ["pepperoni", "salchicha"]
+);
